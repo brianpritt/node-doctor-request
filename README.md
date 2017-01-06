b2b320196e22bf10f3da9ec4aad7058b6febe008
Brian Pritt <brianpritt@gmail.com>
2017-01-06 15:12:39 -0800

    fix minor bugs, add readme.md

---
+++ README.md
@@ -0,0 +1,39 @@
+# Find Your Doctor
+
+#### _Application has user input an ailment, and returns list of doctors who treat said ailment {1/6/2017}_
+
+#### By _**Brian Pritt**_
+
+## Description
+
+This Application is written primarily in JavaScript, using Node.js.  It pulls data from the Better Doctors API. (https://developer.betterdoctor.com/)
+
+## Setup/Installation Requirements
+* This application assumes that you already have Node and Ruby install on your local machine. You can find installation instruction here https://nodejs.org/en/, and here https://www.ruby-lang.org/en/
+* Clone this project directory: https://github.com/brianpritt/node-doctor-request
+* follow the rest of the instructions in your console
+* $ npm install
+* $ npm init
+* $ bower install
+* $ npm build
+* $ npm serve
+* Default browser will automatically open to application.
+
+
+## Known Bugs
+
+_There were no know bugs at time of publication_
+
+## Support and contact details
+
+_Developers Github: http://github.com/brianpritt_
+
+## Technologies Used
+
+This application relies on NODE.js, Ruby, JavaScript, SASS, and Bower.
+
+### License
+
+*Licensed under GPLv3*
+
+Copyright (c) 2016 **_Brian Pritt_**
--- js/doctor-list-interface.js
+++ js/doctor-list-interface.js
@@ -5,7 +5,7 @@ var displayResults = function(results){
   var doctorContact = [];
   console.log(results);
   for(i=0;i<results.data.length;i++){
-    doctorList.push(results.data[i].profile)
+    doctorList.push(results.data[i].profile);
     doctorContact.push(results.data[i].practices[0].phones[0].number);
   };
   if (doctorList.length > 0 ){
@@ -13,19 +13,19 @@ var displayResults = function(results){
       var doctorCount =0;
       $("#results").show();
       $("#results").append("<img class ='profile-pic' src=" + eachDr.image_url+ ">" +"<p class='dr-name'> Dr.    " + eachDr.last_name + ", " + eachDr.title +" </p><p>Contact: " + doctorContact[doctorCount]+"</p><p class='bio'>" + eachDr.bio + "</p><hr>");
-      doctorCount +=1
-    })
+      doctorCount +=1;
+    });
   } else{
-    $("#results").append("<h3>There are no doctors that match your search criteria.  Please try a different symptom, or check your spelling.</h3>")
+    $("#results").append("<h3>There are no doctors that match your search criteria.  Please try a different symptom, or check your spelling.</h3>");
   }
 }
 $(document).ready(function(){
   $("#get-doctor").click(function(){
-    $("#results").text("")
+    $("#results").text("");
     var firstName = $("#first-name");
     var lastName = $("#last-name");
     var ailment = $("#ailment").val();
     var currentDoctorList = new Doctors(firstName,lastName, ailment);
-    currentDoctorList.getSymptom(displayResults)
+    currentDoctorList.getSymptom(displayResults);
   });
 });
